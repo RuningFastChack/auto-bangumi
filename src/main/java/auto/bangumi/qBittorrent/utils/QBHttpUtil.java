1cf855b8182cb606e857b0b8deff78e9e3fa6b48
查查 <903960669@qq.com>
2025-10-08 12:32:03 +0800

    修改QBTOken

--- src/main/java/auto/bangumi/qBittorrent/utils/QBHttpUtil.java
+++ src/main/java/auto/bangumi/qBittorrent/utils/QBHttpUtil.java
@@ -10,7 +10,6 @@ import org.apache.http.Header;
 import org.apache.http.HeaderElement;
 import org.apache.http.HttpResponse;
 
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,9 +24,6 @@ public abstract class QBHttpUtil {
 
     private static String SID = "";
     private static String URL = "";
-    private static Date EXPIRE_TIME = null;
-    // 有效时长
-    private static final long EXPIRE_DURATION = 30 * 60 * 1000;
 
     static {
         UserConfig.DownLoadSetting downLoadSetting = ConfigCatch.findConfig().getDownLoadSetting();
@@ -39,27 +35,23 @@ public abstract class QBHttpUtil {
      * 登录账号
      */
     private static void LoginQBittorrent(String username, String password) {
-        if (SID == null || SID.isEmpty() || EXPIRE_TIME == null || new Date().after(EXPIRE_TIME)) {
-            HttpResponse httpResponse = HttpClientUtil.sendFormPost(URL, QBittorrentPathConstant.LOGIN,
-                    Map.of("Referer", StrUtil.format("{}{}", URL, QBittorrentPathConstant.LOGIN)),
-                    new HashMap<>(),
-                    Map.of("username", username, "password", password));
-            if (httpResponse != null) {
-                int status = httpResponse.getStatusLine().getStatusCode();
-                if (status == 200) {
-                    for (Header header : httpResponse.getAllHeaders()) {
-                        for (HeaderElement element : header.getElements()) {
-                            boolean HAS_SID = element.getName().equals("SID");
-                            if (HAS_SID) {
-                                SID = element.getValue();
-                            }
+        HttpResponse httpResponse = HttpClientUtil.sendFormPost(URL, QBittorrentPathConstant.LOGIN,
+                Map.of("Referer", StrUtil.format("{}{}", URL, QBittorrentPathConstant.LOGIN)),
+                new HashMap<>(),
+                Map.of("username", username, "password", password));
+        if (httpResponse != null) {
+            int status = httpResponse.getStatusLine().getStatusCode();
+            if (status == 200) {
+                for (Header header : httpResponse.getAllHeaders()) {
+                    for (HeaderElement element : header.getElements()) {
+                        boolean HAS_SID = element.getName().equals("SID");
+                        if (HAS_SID) {
+                            SID = element.getValue();
                         }
                     }
-                    // 登录成功后刷新过期时间
-                    EXPIRE_TIME = new Date(System.currentTimeMillis() + EXPIRE_DURATION);
-                } else {
-                    log.error("QBittorrent登录失败，状态码：{}", status);
                 }
+            } else {
+                log.error("QBittorrent登录失败，状态码：{}", status);
             }
         }
     }
